03740f5261e7b0a2ad800a8f6fbcc12c68dcc468
Park Junyoung <bloomwayz@snu.ac.kr>
2025-10-02 02:21:30 +0900

    :sparkles: Implement JSON response types

---
+++ src/types.ts
@@ -0,0 +1,30 @@
+export type SinglePreview = {
+  name: string;
+  difficulty: string;
+  tags: string[];
+  image: string;
+}
+
+export type SingleRecipe = {
+  id: number;
+  name: string;
+  ingredients: string[];
+  instructions: string[];
+  prepTimeMinutes: string[];
+  cookTimeMinutes: number;
+  servings: number;
+  difficulty: string;
+  cuisine: string;
+  caloriesPerServing: number;
+  tags: string[];
+  userId: number;
+  image: string;
+  rating: number;
+  reviewCount: number;
+  mealType: string[]
+}
+
+export type MultipleRecipes = {
+  recipes: SingleRecipe[];
+  total: number
+}

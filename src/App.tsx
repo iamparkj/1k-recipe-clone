3223ed804fc4e11d36aa3e242382d969c456e411
Park Junyoung <bloomwayz@snu.ac.kr>
2025-10-02 02:21:50 +0900

    :sparkles: Show a single preview card

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,33 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import RecipeCard from './RecipeCard';
+import type { MultipleRecipes, SinglePreview } from './types';
+
+const dummyProps: SinglePreview = {
+  name: "",
+  difficulty: "",
+  tags: [],
+  image: ""
+};
 
 const App = () => {
-  const [count, setCount] = useState(0);
+  const [page, setPage] = useState(1);
+  const [props, setProps] = useState(dummyProps);
+
+  useEffect(() => {
+    fetch(`https://dummyjson.com/recipes?limit=12&select=name,difficulty,tags,image`)
+      .then((res) => res.json())
+      .then((data: MultipleRecipes) => {
+        console.info(data);
+        setProps(data.recipes[0]);
+      })
+      .catch((error) => {
+        console.error('Unexpected Error: ', error);
+      }) 
+  }, []);
 
   return (
     <div>
-      <p>Hello World!</p>
-      <button onClick={() => setCount((count) => count + 1)}>
-        Count is {count}
-      </button>
+      <RecipeCard {...props} />
     </div>
   );
 };

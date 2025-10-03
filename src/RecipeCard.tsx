20f4ea3a98a4d3f967ac1d9943ecc3b10ac0434e
Park Junyoung <bloomwayz@snu.ac.kr>
2025-10-04 03:22:06 +0900

    :sparkles: Implement a RecipeCard component

--- src/RecipeCard.tsx
+++ src/RecipeCard.tsx
@@ -4,18 +4,28 @@ const RecipeCard = (
   { name, difficulty, tags, image }: SinglePreview
 ) => {
   return (
-    <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700">
+    <div className="max-w-sm bg-white rounded-2xl">
       <a href="#">
-        <img className="aspect-2/1 w-full rounded-t-lg object-cover" src={image} alt="" />
+        <img className="aspect-2/1 w-full rounded-t-2xl object-cover" src={image} alt="" />
       </a>
-      <div className="p-5">
-        <a href="#">
-          <h6 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-            {name}
-          </h6>
-          <p>{difficulty}</p>
-        </a>
-        { /* TODO: Render tags of the recipe */ }
+      <div className="px-4 pt-4 pb-2 flex place-content-between items-center">
+        <span className="mb-2 text-xl font-semibold tracking-tight text-gray-900">
+          {name}
+        </span>
+        <span className="text-sm text-[#3478f6]">
+          {difficulty}
+        </span>
+      </div>
+      <div className="px-4 pb-4 flex place-content-start gap-2">
+        {
+          tags.map((tag: string) => (
+            <div className="rounded-sm border border-[#efeff0]">
+              <p className="mx-2 my-0.5 text-xs text-[#5c625e]">
+                {tag}
+              </p>
+            </div>
+          ))
+        }
       </div>
     </div>
   );

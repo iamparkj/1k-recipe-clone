9b7a2a90be182fe68f0409be1b8fd2687d405938
Park Junyoung <bloomwayz@snu.ac.kr>
2025-10-04 05:50:48 +0900

    :zap: Add a link to go home

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,9 +1,11 @@
+import { Link } from 'react-router-dom';
+
 const Header = () => {
   return (
     <nav className="flex items-center justify-center px-2 py-4 w-full h-20 text-2xl font-bold shadow-[0_4px_6px_rgba(0,0,0,0.1)]">
-      {/* <a href="/" data-discover="true"> */}
-      <h1>천개의 레시피</h1>
-      {/* </a> */}
+      <Link to="/">
+        <h1>천개의 레시피</h1>
+      </Link>
     </nav>
   )
 }
